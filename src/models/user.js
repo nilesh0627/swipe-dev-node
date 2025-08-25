e1cb01cc23291b52f6d58a39186ba4cfbe0f64db
Nilesh Mishra <nilesh0627@gmail.com>
2025-08-25 13:59:41 +0530

    api changes + validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,63 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     firstName: {
-        type: String
+      type: String,
+      required: true,
+      minLength: 4,
     },
     lastName: {
-        type: String
+      type: String,
     },
     email: {
-        type: String
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(email) {
+        if (!validator.isEmail(email)) throw new Error("Not a valid email");
+      },
     },
     password: {
-        type: String
+      type: String,
+      required: true,
+      maxLength: 20,
+      minLength: 8,
     },
     age: {
-        type: Number
+      type: Number,
+      min: 18,
     },
     gender: {
-        type: String
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value?.toLowerCase())) {
+          throw new Error("Gender not valid");
+        }
+      },
+    },
+    skills: {
+      type: [String],
+      validate(skillsList) {
+        if (skillsList.length > 5) {
+          throw new Error("Maxium 5 skills can be added for a user");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "Default value",
+    },
+    photoUrl: {
+      type: String,
+      validate(url) {
+        if (!validator.isURL(url)) throw new Error("Url not valid");
+      },
     },
     // address: {
     //     street: { type: String },
@@ -28,8 +66,12 @@ const userSchema = new Schema({
     //     zip: { type: String },
     //     country: { type: String }
     // }
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-export { User }
+export { User };
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const ALLOWED_FIELDS_TO_UPDATE = [
+  "firstName",
+  "lastName",
+  "gender",
+  "skills",
+  "age",
+  "password",
+  "photoUrl",
+];
